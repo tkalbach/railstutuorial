eaeaece45f752faf3f3d8b399bce0deddd4a20be
Tyler Kalbach <boarder7395@hotmail.com>
2016-12-17 19:57:30 +0000

    Toy App

--- toy_app/app/controllers/application_controller.rb
+++ toy_app/app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "Hello, World!"
+    #this renders views/application/hello.html.erb by default.
+    #render html: "Hello, World!"
   end
 end
